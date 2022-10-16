f259d035a7f8099a743a6e475464146b7e62bdb6
Francisco Marques Devilla <nanotech@hotmail.com.br>
2022-10-16 13:27:34 -0300

    update

---
+++ aula04/ex008.js
@@ -0,0 +1,7 @@
+let vel = 59
+console.log(`A velocidade de seu carro é ${vel}Km/h`)
+if ( vel > 60) {
+    console.log(`Velocidade acima do permitido. MULTADO!`)
+}
+
+console.log(`Dirija sempre com cinto de segurança!`)
