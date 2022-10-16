65dce555a9acc09ced1a59e4bcee9ccdeb822115
Francisco Marques Devilla <nanotech@hotmail.com.br>
2022-10-16 13:45:24 -0300

    update

--- aula04/script_votação.js
+++ aula04/script_votação.js
@@ -38,3 +38,47 @@ let hora = data.getHours()
         posMsg.innerHTML = 'Boa Noite! 😴'
     }
 }
+
+function resposta() {
+    let agora = new Date()
+    let diaSem = agora.getDay()
+    let diadasemana = agora.getDate()
+    let res = window.document.querySelector('div#resp')
+
+    switch (diaSem) {
+        case 0: {
+            res.innerHTML = `Domingo, dia ${diadasemana}.`
+        }
+            break
+
+        case 1: {
+            res.innerHTML = `Segunda`
+        }
+            break
+
+        case 2: {
+            res.innerHTML = `Terça`
+        }
+            break
+
+        case 3: {
+            res.innerHTML = `Quarta`
+        }
+            break
+
+        case 4: {
+            res.innerHTML = `Quinta`
+        }
+            break
+
+        case 5: {
+            res.innerHTML = `Sexta`
+        }
+            break
+
+        case 6: {
+            res.innerHTML = `Sabado`
+        }
+            break  
+    }
+}
